1a82b827fec30433e23a89f7f15cb4727b2ea10d
zephyril <aochen.jli@gmail.com>
2020-08-26 13:13:45 -0700

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-import { Layout, Drawer, Button, Space } from 'antd';
-import { CloseOutlined, MenuOutlined } from '@ant-design/icons';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
-import { Context as ResponsiveContext } from 'react-responsive'
-import '../styles/my-theme.less';
-import CustomMenu from '../components/CustomMenu';
-import { CenteredText, ContentWrapper } from '../components/Blocks'
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import { Layout, Drawer, Button, Space } from "antd";
+import { CloseOutlined, MenuOutlined } from "@ant-design/icons";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import { Context as ResponsiveContext } from "react-responsive";
+import "../styles/my-theme.less";
+import CustomMenu from "../components/CustomMenu";
+import { CenteredText, ContentWrapper } from "../components/Blocks";
 const { Header, Content, Footer } = Layout;
 
 const Flexbox = styled.div`
@@ -18,7 +18,7 @@ const Flexbox = styled.div`
 `;
 
 const StyledHeader = styled(Header)`
-  background: ${props => props.theme === "light" ? "#FFFFFF" : "#001529"};
+  background: ${(props) => (props.theme === "light" ? "#FFFFFF" : "#001529")};
 
   .header-content {
     padding: 0 1em 0 1em;
@@ -66,11 +66,11 @@ const MediaQuery = styled.div`
 
 function MyApp({ Component, pageProps }) {
   const ROUTES = [
-    {route: '/', name: 'Home'},
+    { route: "/", name: "Home" },
     // {route: '/curriculum', name: 'Curriculum'},
-    {route: '/the-team', name: 'The Team'},
-    {route: '/testimonials', name: 'Testimonials'},
-    {route: '/contact', name: 'Contact'},
+    { route: "/the-team", name: "The Team" },
+    { route: "/testimonials", name: "Testimonials" },
+    { route: "/contact", name: "Contact" },
   ];
 
   const router = useRouter();
@@ -86,16 +86,28 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Layout>
         <Drawer
-          title={<Button onClick={() => toggleDrawer()}><CloseOutlined /></Button>}
+          title={
+            <Button onClick={() => toggleDrawer()}>
+              <CloseOutlined />
+            </Button>
+          }
           placement="right"
           closable={false}
           onClose={onClose}
-          visible={visible}>
-          <CustomMenu routes={ROUTES} theme="light" mode="vertical" onClick={toggleDrawer} defaultSelectedKeys={[router.pathname]}/>
+          visible={visible}
+        >
+          <CustomMenu
+            routes={ROUTES}
+            theme="light"
+            mode="vertical"
+            onClick={toggleDrawer}
+            defaultSelectedKeys={[router.pathname]}
+          />
         </Drawer>
         <StyledHeader theme="light">
           <Flexbox>
@@ -103,8 +115,16 @@ function MyApp({ Component, pageProps }) {
               <Logo>Project Ignite</Logo>
               <StyledSpace direction="horizontal">
                 <MediaQuery>
-                  <Button className="mobile" onClick={() => toggleDrawer()}><MenuOutlined /></Button>
-                  <CustomMenu className="desktop" routes={ROUTES} theme="light" mode="horizontal" defaultSelectedKeys={[router.pathname]}/>
+                  <Button className="mobile" onClick={() => toggleDrawer()}>
+                    <MenuOutlined />
+                  </Button>
+                  <CustomMenu
+                    className="desktop"
+                    routes={ROUTES}
+                    theme="light"
+                    mode="horizontal"
+                    defaultSelectedKeys={[router.pathname]}
+                  />
                 </MediaQuery>
               </StyledSpace>
             </div>
@@ -112,7 +132,7 @@ function MyApp({ Component, pageProps }) {
         </StyledHeader>
         <Flexbox>
           <StyledContent>
-              <Component {...pageProps} />
+            <Component {...pageProps} />
           </StyledContent>
         </Flexbox>
         <Footer>
@@ -123,4 +143,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default MyApp
+export default MyApp;
